4a03b850996074884ff7c1182793a83963dc95b0
Immanuel Haffner <haffner.immanuel@gmail.com>
2016-05-24 18:41:24 +0200

    fixed home dir (~ was not expanded, use $HOME instead)

--- install-vim_plug.sh
+++ install-vim_plug.sh
@@ -1,7 +1,10 @@
+FILE="$HOME/.config/nvim/autoload/plug.vim"
+
 #!/bin/bash
-if [ ! -f "~/.config/nvim/autoload/plug.vim" ]; then
+if [ ! -f "$FILE" ]; then
     echo "Installing vim-plug";
-    curl -fLo "~/.config/nvim/autoload/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
+    mkdir -p $(dirname "$FILE")
+    curl -fLo "$FILE" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
 else
     echo "vim-plug already installed"
 fi
