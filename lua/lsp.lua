2ef9e5e63554704b84621e624416dba2b12f10de
Immanuel Haffner <haffner.immanuel@gmail.com>
2022-10-06 09:07:47 +0200

    Add bashls language server for Bash scripts.

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -99,6 +99,11 @@ function M.setup()
             }
         }
     }
+
+    require('lspconfig')['bashls'].setup{
+        on_attach = on_attach,
+        capabilities = require'cmp_nvim_lsp'.update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    }
     --}}}---------------------------------------------------------------------------------------------------------------
 
 end
