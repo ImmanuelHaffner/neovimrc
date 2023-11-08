41c6bc42ac09a9a9b03443dac7f03776f3a3041e
Immanuel Haffner <haffner.immanuel@gmail.com>
2023-11-08 09:18:36 +0100

    Switch to color scheme "Night Owl".
    Night Owl provides proper semantic highlight groups.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,4 @@
 return {
-    { 'overcache/NeoSolarized', priority = 1000, config = function() vim.cmd[[colorscheme NeoSolarized]] end },
+    { 'overcache/NeoSolarized', priority = 1000, config = function() --[[ vim.cmd.colorscheme('NeoSolarized') ]] end },
+    { 'oxfist/night-owl.nvim', priority = 1000, config = function() vim.cmd.colorscheme('night-owl') end, },
 }
