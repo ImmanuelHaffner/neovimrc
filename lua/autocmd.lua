08b8072eb1fcfd5a7aaea14912f0d92faf500fd2
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-02-09 11:45:05 +0100

    Disable the vimtip autocmd for now.
    It was funny and fancy in the beginning, but now it's just annoying me
    and often in the way.

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -11,23 +11,23 @@ function M.setup()
     --}}}---------------------------------------------------------------------------------------------------------------
 
     ----- Show tip on launch {{{----------------------------------------------------------------------------------------
-    vim.api.nvim_create_autocmd('VimEnter', {
-        group = vim.api.nvim_create_augroup('vimtip', {clear=true}),
-        callback = function()
-            local job = require'plenary.job'
-            job:new({
-                command = 'curl',
-                args = { 'https://vtip.43z.one' },
-                on_exit = function(j, exit_code)
-                    local res = table.concat(j:result())
-                    if exit_code ~= 0 then
-                        res = 'Error fetching tip: ' .. res
-                    end
-                    vim.notify(res, 2, { title = 'Tip!' })
-                end,
-            }):start()
-        end,
-    })
+    -- vim.api.nvim_create_autocmd('VimEnter', {
+    --     group = vim.api.nvim_create_augroup('vimtip', {clear=true}),
+    --     callback = function()
+    --         local job = require'plenary.job'
+    --         job:new({
+    --             command = 'curl',
+    --             args = { 'https://vtip.43z.one' },
+    --             on_exit = function(j, exit_code)
+    --                 local res = table.concat(j:result())
+    --                 if exit_code ~= 0 then
+    --                     res = 'Error fetching tip: ' .. res
+    --                 end
+    --                 vim.notify(res, 2, { title = 'Tip!' })
+    --             end,
+    --         }):start()
+    --     end,
+    -- })
     --}}}---------------------------------------------------------------------------------------------------------------
 
     ----- Reload changed files {{{--------------------------------------------------------------------------------------
