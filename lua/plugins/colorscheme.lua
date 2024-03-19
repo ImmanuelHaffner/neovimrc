257d8ac285a09bff6e7a9128c9c53e3a49c85118
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-03-19 11:55:49 +0100

    Add colorscheme dracula

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,5 @@
 return {
     { 'overcache/NeoSolarized', priority = 1000, config = function() --[[ vim.cmd.colorscheme('NeoSolarized') ]] end },
+    { 'binhtran432k/dracula.nvim', priority = 1000, config = function() --[[ vim.cmd.colorscheme('dracula') ]] end, },
     { 'oxfist/night-owl.nvim', priority = 1000, config = function() vim.cmd.colorscheme('night-owl') end, },
 }
