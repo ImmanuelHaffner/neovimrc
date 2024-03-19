004e48896fad1ec9ae86d94b1dd1bee21fd52006
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-03-19 14:24:04 +0100

    Add plugin before to navigate edit locations

---
+++ lua/plugins/before.lua
@@ -0,0 +1,28 @@
+return {
+    { 'bloznelis/before.nvim',
+        dependencies = {
+            'folke/which-key.nvim',
+            'nvim-telescope/telescope.nvim',
+        },
+        config = function()
+            local before = require('before')
+            before.setup()
+
+            local wk = require'which-key'
+            wk.register({
+                ['['] = {
+                    e = { before.jump_to_last_edit, 'Jump to last edit location' },
+                },
+                [']'] = {
+                    e = { before.jump_to_next_edit, 'Jump to next edit location' },
+                },
+                ['<leader>'] = {
+                    e = { before.show_edits_in_quickfix, 'Show edits in Quickfix window' },
+                    f = {
+                        e = { before.show_edits_in_telescope, 'Show edits in Telescope' },
+                    }
+                }
+            }, { silent = true })
+        end
+}
+}
