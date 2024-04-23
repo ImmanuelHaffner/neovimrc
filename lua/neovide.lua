3e55a4f436010ffb0ec9645f9e4f53db275aedd0
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-04-23 16:28:07 +0200

    Add key bindings to change font size in neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,8 +1,35 @@
+vim.g.gui_font_face = 'Source Code Pro'
+vim.g.gui_font_size = 11
+
 local M = {}
 
+function M.refresh_gui_font()
+    vim.o.guifont = string.format("%s:h%d", M.gui_font_face, M.gui_font_size)
+end
+
+function M.resize_gui_font(delta)
+    M.gui_font_size = M.gui_font_size + delta
+    M.refresh_gui_font()
+end
+
+function M.reset_gui_font()
+    M.gui_font_face = vim.g.gui_font_face
+    M.gui_font_size = vim.g.gui_font_size
+    M.refresh_gui_font()
+end
+
+
 function M.setup()
-    vim.o.guifont = 'Source Code Pro:h11' -- text below applies for VimScript
+    M.reset_gui_font()
     vim.g.neovide_hide_mouse_when_typing = true
+
+    -- Neovide keymaps
+    local wk = require'which-key'
+    wk.register({
+        ['<C-+>'] = { function() M.resize_gui_font(1) end, 'increase font size' },
+        ['<C-->'] = { function() M.resize_gui_font(-1) end, 'increase font size' },
+        ['<C-=>'] = { function() M.reset_gui_font() end, 'increase font size' },
+    }, { mode = { 'n', 'i' }, noremap = true, silent = true })
 end
 
 return M
