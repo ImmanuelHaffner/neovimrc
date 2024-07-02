6b71c2a8c1cdca229e4e99383556081fbdb7ec75
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-07-02 12:26:31 +0200

    Tune noice settings
    Ignore some messages with special routes.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -24,11 +24,37 @@ return {
             },
             -- you can enable a preset for easier configuration
             presets = {
-                bottom_search = true, -- use a classic bottom cmdline for search
+                bottom_search = false, -- use a classic bottom cmdline for search
                 command_palette = false, -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false, -- add a border to hover docs and signature help
+                lsp_doc_border = true, -- add a border to hover docs and signature help
+            },
+            messages = {
+                -- NOTE: If you enable messages, then the cmdline is enabled automatically.  This is a current Neovim
+                -- limitation.
+                enabled = true, -- enables the Noice messages UI
+                view = "mini", -- default view for messages
+                view_error = "popup", -- view for errors
+                view_warn = "popup", -- view for warnings
+                view_history = "messages", -- view for :messages
+                view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+            },
+            routes = {
+                {
+                    filter = {
+                        event = "msg_show",
+                        kind = "",
+                    },
+                    opts = { skip = true },
+                },
+                {
+                    filter = {
+                        event = "msg_show",
+                        kind = "search_count",
+                    },
+                    opts = { skip = true },
+                },
             },
             views = {
                 cmdline_popup = {
