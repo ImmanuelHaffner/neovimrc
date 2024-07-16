f9179c5fe7a05029d210402f21ba0afa093598b2
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-07-16 14:20:55 +0200

    Update telescope
    This fixes the annoying scroll offset bug

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
         build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
     },
     { 'nvim-telescope/telescope.nvim',
-        tag = '0.1.2',
+        tag = '0.1.8',
         dependencies = {
             'nvim-lua/plenary.nvim',
             'nvim-telescope/telescope-fzf-native.nvim',
