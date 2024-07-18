6970c07ac86252921fb880e17910032e7b02e0b5
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-07-18 11:41:29 +0200

    Fix diagnostic navigation key mapping

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -75,7 +75,7 @@ return {
                 -- Diagnostics navigation
                 vim.keymap.set('n', '?', vim.diagnostic.open_float, opts)
                 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-                vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
+                vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
                 -- wk.add{
                 --     {
                 --         buffer = bufnr,
