84924199ed7841edf6ed868f3f069260b446e5e6
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-08-07 16:53:05 +0200

    Add codesnap plugin

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,11 @@
+return {
+    { 'mistricky/codesnap.nvim',
+        build = 'make',
+        opts = {
+            mac_window_bar = false,
+            has_line_number = false,
+            bg_padding = 0,
+            code_font_family = 'Source Code Pro',
+        },
+    },
+}
