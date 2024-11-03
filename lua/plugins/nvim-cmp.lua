feeb9cb97fa34a634967d69a6c65865d0c2896fb
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-11-03 10:54:46 +0200

    Update nvim-cmp to have sane completion with max length entries

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -5,6 +5,7 @@ return {
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
+            'hrsh7th/cmp-omni',
             'petertriho/cmp-git',
             'https://git.sr.ht/~p00f/clangd_extensions.nvim',
         },
@@ -13,9 +14,19 @@ return {
             cmp.setup{
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
+                    {
+                        name = 'omni',
+                        option = {
+                            disable_omnifuncs = { 'v:lua.vim.lsp.omnifunc' }
+                        }
+                    },
                 }, {
                     { name = 'buffer' },
                 }),
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
                 mapping = cmp.mapping.preset.insert{
                     ['<C-b>'] = cmp.mapping.scroll_docs(8),
                     ['<C-f>'] = cmp.mapping.scroll_docs(-8),
@@ -39,10 +50,30 @@ return {
                     },
                 },
                 formatting = {
-                    format = function(entry, vim_item)
-                        vim_item.abbr = string.sub(vim_item.abbr, 1, 40)
-                        return vim_item
+                    -- format = function(entry, vim_item)
+                    --     vim_item.abbr = string.sub(vim_item.abbr, 1, 40)
+                    --     return vim_item
+                    -- end
+                    format = function(_, item)
+                        local win_width = vim.api.nvim_win_get_width(0)
+
+                        local truncate = function(str, frac)
+                            local max_len = math.floor(frac * win_width)
+                            if #str > max_len then
+                                return vim.fn.strcharpart(str, 0, max_len - 1) .. '…'
+                            else
+                                return str
+                            end
+                        end
+
+                        item.abbr = truncate(item.abbr, .2)
+                        item.menu = truncate(item.menu, .3)
+
+                        return item
                     end
+                },
+                completion = {
+                    autocomplete = false,
                 },
             }
             cmp.setup.filetype('gitcommit', {
@@ -53,5 +84,13 @@ return {
                 })
             })
         end,
+        keys = {
+            {
+                '<C-x><C-o>',
+                function() require'cmp'.complete() end,
+                desc = 'Auto-complete',
+                mode = 'i',
+            }
+        }
     },
 }
