c2528b3b67e24a36737c80c0b9c3c43fe5416a56
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-11-04 09:42:48 +0200

    Notify use lazy keymap

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,17 +1,12 @@
 return {
     { 'rcarriga/nvim-notify',
-        dependencies = {
-            'folke/which-key.nvim',
-        },
         config = function()
             vim.opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
             vim.notify = require'notify'
-
-            local wk = require'which-key'
-            wk.add{
-                { '<leader>n', group = 'Notify' },
-                { '<leader>nd', function() require'notify'.dismiss() end, desc = 'Dismiss all notifications' },
-            }
         end,
+        keys = {
+            { '<leader>n', group = 'Notify' },
+            { '<leader>nd', function() require'notify'.dismiss() end, desc = 'Dismiss all notifications' },
+        }
     }
 }
