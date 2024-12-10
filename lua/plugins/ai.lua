2e80123f6e9aeb589944bf714c76ceff22e520f6
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-12-10 23:37:47 +0100

    Add first AI assistant NeoCodeium
    The key bindings don't feel natural yet... let's see

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,47 @@
+return {
+    {
+        'monkoose/neocodeium',
+        dependencies = {
+            'folke/which-key.nvim',
+        },
+        event = 'VeryLazy',
+        config = function()
+            local neocodeium = require'neocodeium'
+            neocodeium.setup{
+                enabled = false,  -- don't enable by default
+                filetypes = {
+                    TelescopePrompt = false,
+                    ["dap-repl"] = false,
+                },
+            }
+
+            local cmd = require'neocodeium.commands'
+            require'which-key'.add{
+                { '<leader>a', group = 'AI Tools' },
+                { '<leader>an', group = 'NeoCodeium' },
+                { '<leader>ant', cmd.toggle, desc = 'Toggle NeoCodeium globally' },
+                { '<leader>anb', cmd.toggle_buffer, desc = 'Toggle NeoCodeium for current buffer' },
+                { '<leader>ac', neocodeium.chat, desc = 'NeoCodeium chat' },
+            }
+
+            vim.keymap.set("i", "<A-a>", function()
+                require("neocodeium").accept()
+            end)
+            vim.keymap.set("i", "<A-w>", function()
+                require("neocodeium").accept_word()
+            end)
+            vim.keymap.set("i", "<A-l>", function()
+                require("neocodeium").accept_line()
+            end)
+            vim.keymap.set("i", "<A-n>", function()
+                require("neocodeium").cycle_or_complete()
+            end)
+            vim.keymap.set("i", "<A-p>", function()
+                require("neocodeium").cycle_or_complete(-1)
+            end)
+            vim.keymap.set("i", "<A-c>", function()
+                require("neocodeium").clear()
+            end)
+        end,
+    }
+}
