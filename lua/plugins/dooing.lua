dd340baf49d664964b8d1578edc5eff0f90f0203
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-12-10 23:37:08 +0100

    Refactor dooing config

--- lua/plugins/dooing.lua
+++ lua/plugins/dooing.lua
@@ -5,13 +5,13 @@ return {
             'folke/which-key.nvim',
         },
         config = function()
-            local wk = require'which-key'
             require'dooing'.setup{
                 keymaps = {
                     toggle_window = false,
                 },
             }
-            wk.add{
+
+            require'which-key'.add{
                 { '<leader>tn', ':Dooing<cr>', desc = 'Open Todo notes' },
             }
         end,
