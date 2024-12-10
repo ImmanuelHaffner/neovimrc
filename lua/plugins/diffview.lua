ec6e527218cd10f9d9a366432632c0ece9fab3ed
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-12-10 23:36:46 +0100

    Add diffview config and key bindings

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,4 +1,16 @@
 return {
     { 'sindrets/diffview.nvim',
+        dependencies = {
+            'folke/which-key.nvim',
+        },
+        config = function()
+            vim.opt.fillchars:append{ diff = '╱' }
+
+            require'which-key'.add{
+                { '<leader>gd', group = 'Git Diffview …' },
+                { '<leader>gdv', '<cmd>DiffviewOpen -uno<cr>', desc = 'Open Diffview' },
+                { '<leader>gdh', '<cmd>DiffviewFileHistory %<cr>', desc = 'File history' },
+            }
+        end,
     }
 }
