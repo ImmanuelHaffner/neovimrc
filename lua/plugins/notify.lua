848713bf2058992478360ea2ad5e4c5a0aab8b23
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-12-13 20:01:24 +0100

    Fix group name of notify keymap

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,12 +1,15 @@
 return {
     { 'rcarriga/nvim-notify',
+        dependencies = {
+            'folke/which-key.nvim',
+        },
         config = function()
             vim.opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
             vim.notify = require'notify'
+            require'which-key'.add{
+                { '<leader>n', group = 'Notify' },
+                { '<leader>nd', function() require'notify'.dismiss() end, desc = 'Dismiss all notifications' },
+            }
         end,
-        keys = {
-            { '<leader>n', group = 'Notify' },
-            { '<leader>nd', function() require'notify'.dismiss() end, desc = 'Dismiss all notifications' },
-        }
     }
 }
