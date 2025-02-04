f94c3b6138f975abf5b9f797392bf0a6b7e6e3a4
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-02-04 18:39:51 +0200

    Update and configure gitsigns
    Still need to find a way to present unstaged changes in a nicer way.

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,39 +1,50 @@
 return {
     { 'lewis6991/gitsigns.nvim',
-        tag = 'v0.9.0',
+        branch = 'main',
         dependencies = { 'folke/which-key.nvim' },
         config = function()
             require'gitsigns'.setup{
+                signs = {
+                    add          = { text = '┃' },
+                    change       = { text = '┃' },
+                    delete       = { text = '␡' },
+                    topdelete    = { text = '␡' },
+                    changedelete = { text = '✎' },
+                    untracked    = { text = '┆' },
+                },
+                signs_staged = {
+                    add          = { text = '┃' },
+                    change       = { text = '┃' },
+                    delete       = { text = '␡' },
+                    topdelete    = { text = '␡' },
+                    changedelete = { text = '✎' },
+                    untracked    = { text = '┆' },
+                },
+                signs_staged_enable = true,
                 on_attach = function(bufnr)
-                    local gs = package.loaded['gitsigns']
+                    local gs = require'gitsigns'
                     local wk = require'which-key'
 
-                    local function map(mode, l, r, opts)
-                        opts = opts or {}
-                        opts.buffer = bufnr
-                        vim.keymap.set(mode, l, r, opts)
-                    end
-
                     -- Navigation
-                    wk.add{
+                    wk.add({
                         { ']h', function()
                                 if vim.wo.diff then return ']c' end
-                                vim.schedule(function() gs.next_hunk() end)
+                                vim.schedule(function() gs.nav_hunk'next' end)
                                 return '<Ignore>'
                             end,
                             desc = 'Next Git hunk'
                         },
                         { '[h', function()
                                 if vim.wo.diff then return ']c' end
-                                vim.schedule(function() gs.prev_hunk() end)
+                                vim.schedule(function() gs.nav_hunk'prev' end)
                                 return '<Ignore>'
                             end,
                             desc = 'Previous Git hunk'
                         },
-                    }
+                    }, { buffer = bufnr })
 
                     -- Actions
-                    wk.add{
+                    wk.add({
                         { '<leader>g', group = 'Git' },
                         { '<leader>gs', group = 'Gitsign' },
                         {
@@ -41,26 +52,19 @@ return {
                             { '<leader>gss', gs.stage_hunk, desc = 'Stage hunk' },
                             { '<leader>gsr', gs.reset_hunk, desc = 'Reset hunk' },
                         },
-                        { '<leader>gsu', gs.undo_stage_hunk, desc = 'Undo stage hunk' },
+                        { '<leader>gsu', gs.stage_hunk, desc = 'Undo stage hunk' },
                         { '<leader>gsS', gs.stage_buffer, desc = 'Stage buffer' },
                         { '<leader>gsR', gs.reset_buffer, desc = 'Reset buffer' },
                         { '<leader>gsp', gs.preview_hunk, desc = 'Preview hunk' },
                         { '<leader>gsb', function() gs.blame_line{full=false} end, desc = 'Blame current line' },
                         { '<leader>gsd', gs.diffthis, desc = 'Diff current hunk' },
-                        { '<leader>gsD', function() gs.diffthis('~') end, desc = 'Diff current file' },
+                        { '<leader>gsD', function() gs.diffthis'~' end, desc = 'Diff current file' },
                         {
                             { '<leader>gst', group = 'Toggles' },
                             { '<leader>gstb', gs.toggle_current_line_blame, desc = 'Toggle current line blame' },
-                            { '<leader>gstd', gs.toggle_deleted, desc = 'Toggle deleted' },
+                            { '<leader>gstd', gs.preview_hunk_inline, desc = 'Toggle deleted' },
                         },
-                    }
-
-                    -- Text object
-                    -- wk.add{
-                    --     mode = { 'o', 'x' },
-                    --     { 'g', group = 'Gitsign' },
-                    --     { 'gh', ':<C-U>Gitsigns select_hunk<CR>', desc = 'Gitsign select hunk' },
-                    -- }
+                    }, { buffer = bufnr })
                 end
             }
         end,
