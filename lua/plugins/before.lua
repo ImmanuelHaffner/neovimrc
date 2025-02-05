abfbcc26db1249ee84f5d0a9d34f361824e514a4
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-02-05 15:39:37 +0200

    Fix before.nvim lazy loading because we defined keymaps

--- lua/plugins/before.lua
+++ lua/plugins/before.lua
@@ -3,6 +3,7 @@ return {
         dependencies = {
             'nvim-telescope/telescope.nvim',
         },
+        lazy = false,
         config = function()
             require('before').setup()
             require'telescope'.load_extension('before')
