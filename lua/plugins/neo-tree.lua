20e65a18f83d90db4d45a733b6ee4d376c6f79d6
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-02-05 17:11:22 +0200

    Improve neo-tree configuration
    - Avoid lazy-loading triggerd by keymaps set.
    - Open through lua rather than a vim command

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,7 @@ return {
             'MunifTanjim/nui.nvim',
             '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
         },
+        lazy = false,
         config = function()
             local neotree = require'neo-tree'
             neotree.setup{
@@ -27,7 +28,26 @@ return {
             }
         end,
         keys = {
-            { '<F2>', ':Neotree action=focus reveal<CR>', desc = 'Open Neo-tree (filesystem)' },
+            { '<F2>', function()
+                local reveal_file = vim.fn.expand'%:p'
+                if reveal_file == '' then
+                  reveal_file = vim.fn.getcwd()
+                else
+                  local f = io.open(reveal_file, 'r')
+                  if f then
+                    f.close(f)
+                  else
+                    reveal_file = vim.fn.getcwd()
+                  end
+                end
+                require'neo-tree.command'.execute{
+                  action = 'focus',          -- OPTIONAL, this is the default value
+                  source = 'filesystem',     -- OPTIONAL, this is the default value
+                  position = 'left',         -- OPTIONAL, this is the default value
+                  reveal_file = reveal_file, -- path to file or folder to reveal
+                  reveal_force_cwd = true,   -- change cwd without asking if needed
+                }
+            end, desc = 'Open Neo-tree (filesystem)' },
         }
     },
 }
