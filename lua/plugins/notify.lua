ead6882a6b3c0ceb579f8424c21c0d247a74f329
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-02-06 08:59:23 +0200

    Tune notify dismiss command

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -8,7 +8,10 @@ return {
             vim.notify = require'notify'
             require'which-key'.add{
                 { '<leader>n', group = 'Notify' },
-                { '<leader>nd', function() require'notify'.dismiss() end, desc = 'Dismiss all notifications' },
+                { '<leader>nd', function()
+                    require'notify'.dismiss{ pending=true, silent=true }
+                end,
+                desc = 'Dismiss all notifications' },
             }
         end,
     }
