10f7a460cdaa5777af1b04bea6b4f1914be29dd1
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-02-09 10:25:36 +0200

    Add kind icons to nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -50,6 +50,19 @@ return {
                     --     return vim_item
                     -- end
                     format = function(_, item)
+                        -- Icons setup (Optional, but enhances UI)
+                        local kind_icons = {
+                            Text = '', Method = '', Function = '󰡱', Constructor = '',
+                            Field = '', Variable = '', Class = '', Interface = '',
+                            Module = '', Property = '', Unit = '', Value = '',
+                            Enum = '', Keyword = '', Snippet = '', Color = '',
+                            File = '', Reference = '', Folder = '', EnumMember = '',
+                            Constant = '', Struct = '', Event = '', Operator = '',
+                            TypeParameter = '',
+                        }
+                        -- Add kind icons with highlighting
+                        item.kind = string.format('%s %s', kind_icons[item.kind] or '', item.kind)
+
                         local win_width = vim.api.nvim_win_get_width(0)
 
                         local truncate = function(str, frac)
