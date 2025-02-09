a78ab2d0668032b0794f14aedf89028055fa8a03
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-02-09 13:00:35 +0200

    Add markdown support to notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -5,7 +5,14 @@ return {
         },
         config = function()
             vim.opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
-            vim.notify = require'notify'
+            local notify = require'notify'
+            vim.notify = notify
+            notify.setup{
+                on_open = function(win)
+                    local buf = vim.api.nvim_win_get_buf(win)
+                    vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+                end,
+            }
             require'which-key'.add{
                 { '<leader>n', group = 'Notify' },
                 { '<leader>nd', function()
