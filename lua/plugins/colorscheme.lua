22a492c73fa23acb67c4950b9884c2ab3be6d14b
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-02-09 10:26:36 +0200

    Minor refactor

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,13 +6,14 @@ return {
         config = function()
             vim.cmd.colorscheme'night-owl'
             vim.cmd[[
-                highlight! DiffAdd guifg=#859900 guibg=#002b36
-                highlight! DiffDelete guifg=#848586 guibg=#331c1c
-                highlight! DiffChange guifg=#b58900 guibg=#002b36 gui=NONE cterm=NONE
-                highlight! DiffText guifg=NONE guibg=NONE gui=undercurl guisp=#b58900
-                highlight! ColorColumn guibg=#331c1c
-                highlight! CursorColumn guibg=#2d3a4a
-                highlight! CursorLine guibg=#2d3a4a
+                " Highlight groups for diff view
+                hi! DiffAdd guifg=#859900 guibg=#002b36
+                hi! DiffDelete guifg=#848586 guibg=#331c1c
+                hi! DiffChange guifg=NONE guibg=#002b36 gui=NONE cterm=NONE
+                hi! DiffText guifg=NONE guibg=NONE gui=undercurl guisp=#b58900
+                hi! ColorColumn guibg=#331c1c
+                hi! CursorColumn guibg=#2d3a4a
+                hi! CursorLine guibg=#2d3a4a
             ]]
             vim.opt.fillchars:append{ diff = ' ' }
         end,
