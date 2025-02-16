fafbf7a5b23913b4db4d1fda97986f4fa1562a8b
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-02-16 09:39:05 +0200

    WIP dynamically compute font size based on DPI

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,5 +1,24 @@
+local function get_dpi()
+  local handle = io.popen[[xrdb -query | grep Xft.dpi | awk '{print $2}']]
+  if handle then
+    local result = handle:read'*a'
+    handle:close()
+    return tonumber(result) or 96
+  end
+  return 96  -- Default DPI if not found
+end
+
+
+local function compute_font_size(dpi)
+    -- 169 dpi ⇒ 8 pt
+    -- 69 dpi ⇒ 13 pt
+    return math.floor(-.05 * dpi + 16.45 + .5)  -- +.5 to round
+end
+
+local dpi = get_dpi()
+
 vim.g.gui_font_face = 'Source Code Pro'
-vim.g.gui_font_size = 11
+vim.g.gui_font_size = compute_font_size(dpi)
 
 local M = {}
 
