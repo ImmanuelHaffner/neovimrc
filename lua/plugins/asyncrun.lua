aee982ffffc5357927b1afe7e77ff260b76f6483
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-04-02 22:44:21 +0200

    Call AsyncRun vimscript directly from Lua

--- lua/plugins/asyncrun.lua
+++ lua/plugins/asyncrun.lua
@@ -1,10 +1,13 @@
 return {
     { 'skywind3000/asyncrun.vim',
         config = function()
-            vim.g.asyncrun_exit = 'silent lua require\'galaxyline\'.load_galaxyline()'
+            vim.g.asyncrun_exit = [[silent lua require'galaxyline'.load_galaxyline()]]
         end,
         keys = {
-            { '<F5>', '<cmd>AsyncRun -program=make<cr>', desc = 'Make' },
+            -- { '<F5>', '<cmd>AsyncRun -program=make<cr>', desc = 'Make' },
+            { '<F5>', function()
+                vim.api.nvim_call_function('asyncrun#run', { false, { program = 'make' }, '' })
+            end, desc = 'Make' },
         },
     },
 }
