bc67d1f6142ab16c0f61a0b010c9d39a9a97042c
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-04-02 23:14:14 +0200

    Don't configure icecc
    Currently doesn't work on the home setup.  If icecc isn't set, Firebolt.nvim
    will replace the icecc key bindings by verbose local build.

--- lua/plugins/firebolt.lua
+++ lua/plugins/firebolt.lua
@@ -29,10 +29,10 @@ return {
                     'planner_tests',
                     'sql_parser_tests',
                 },
-                icecc = {
-                    path = '/home/immanuel/Documents/Firebolt/code/packdb/icecc_prefix.sh',
-                    jobs = 200,
-                },
+                -- icecc = {
+                --     path = '/home/immanuel/Documents/Firebolt/code/packdb/icecc_prefix.sh',
+                --     jobs = 200,
+                -- },
                 notify = true,
             },
             test = {
