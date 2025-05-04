3a167f6e574437c666f98fc19196c1b5b758c8a9
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-05-04 13:50:44 +0300

    Enable treesitter highlight unless an attached LSP provides semantic tokens

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,17 @@ return {
     config = function()
         require'nvim-treesitter'.setup{}
         require'nvim-treesitter.configs'.setup{
-            highlight = { enable = false },  -- use NeoVim's LSP-based semantic highlighting
+            highlight = {
+                enable = true,
+                disable = function(lang, buf)
+                    local clients = vim.lsp.get_clients({ bufnr = buf })
+                    for _, client in ipairs(clients) do
+                        if client.server_capabilities.semanticTokensProvider then
+                            return true -- Disable Tree-sitter highlighting if LSP provides semantic tokens
+                        end
+                    end
+                end,
+            },
             incremental_selection = { enable = false },
             indent = { enable = false },  -- again, rely on LSP-based formatting
         }
