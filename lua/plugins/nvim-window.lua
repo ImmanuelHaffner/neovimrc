25f5e03a58c6b8b9c572999f7337328f63671d2b
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-05-26 10:31:50 +0300

    Merge two plugin files with window pickers

--- lua/plugins/nvim-window.lua
+++ lua/plugins/nvim-window.lua
@@ -10,5 +10,21 @@ return {
         keys = {
             { '<leader><leader>', function() require'nvim-window'.pick() end, desc = 'Jump to window' },
         },
-    }
+    },
+    {
+        -- This plugin provides jumps to *all* kinds of windows, including *floating* windows.  This is quite
+        -- undesirable.  Load this plugin for NeoTree, but don't bind it.  Use `yorickpeterse/nvim-window` instead.
+        's1n7ax/nvim-window-picker',
+        name = 'window-picker',
+        event = 'VeryLazy',
+        version = '2.*',
+
+        config = function()
+            require'window-picker'.setup{
+                hint = 'floating-big-letter',
+                show_prompt = false,
+                selection_chars='awftnuyorsei',
+            }
+        end,
+    },
 }
