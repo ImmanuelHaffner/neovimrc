62c38794f90c75607d28e72bc1a577bc9ccc569c
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-06-24 17:15:42 +0200

    Make <F2> truly toggle the NeoTree window

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -42,6 +42,7 @@ return {
                 end
                 require'neo-tree.command'.execute{
                   action = 'focus',          -- OPTIONAL, this is the default value
+                  toggle = true,
                   source = 'filesystem',     -- OPTIONAL, this is the default value
                   position = 'left',         -- OPTIONAL, this is the default value
                   reveal_file = reveal_file, -- path to file or folder to reveal
