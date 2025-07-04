414e3a685af9b71d1965262e0cfe0b0b3da1d90a
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-07-04 21:46:04 +0200

    Change MCP-Hub from bundled to node package
    We rely on a local installation in `~/.local`, which installs into
    `~/.local/node_modules`.  This also requires `~/.local/node_modules/.bin` to be
    in `$PATH`.

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -66,14 +66,10 @@ return {
         },
         -- uncomment the following line to load hub lazily
         --cmd = 'MCPHub',  -- lazy load
-        -- build = 'npm install mcp-hub@latest',  -- Installs required mcp-hub npm module
-        -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
-        build = 'bundled_build.lua',  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+        build = "cd ~/.local && npm install mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
         config = function()
-            require'mcphub'.setup{
-                use_bundled_binary = true,
-            }
-        end,
+            require("mcphub").setup()
+        end
     },
     { 'olimorris/codecompanion.nvim',
         dependencies = {
