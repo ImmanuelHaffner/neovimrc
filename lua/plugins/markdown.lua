273b45bb93dbff6116c0289e4b410c21403e0bbe
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-07-04 19:09:33 +0200

    Fix wrong dependency and load order between markview and treesitter

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -21,10 +21,10 @@ return {
     {
         'OXY2DEV/markview.nvim',
         lazy = false,      -- Recommended
+        priority = 49,
         -- ft = 'markdown' -- If you decide to lazy-load anyway
         dependencies = {
-            'nvim-treesitter/nvim-treesitter',
-            'nvim-tree/nvim-web-devicons'
+            'nvim-tree/nvim-web-devicons',
         },
         opts = {
             preview = {
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,27 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-        require'nvim-treesitter'.setup{}
-        require'nvim-treesitter.configs'.setup{
-            highlight = {
-                enable = true,
-                disable = function(lang, buf)
-                    local clients = vim.lsp.get_clients({ bufnr = buf })
-                    for _, client in ipairs(clients) do
-                        if client.server_capabilities.semanticTokensProvider then
-                            return true -- Disable Tree-sitter highlighting if LSP provides semantic tokens
+    { 'nvim-treesitter/nvim-treesitter',
+        dependencies = {
+            'OXY2DEV/markview.nvim',
+        },
+        lazy = false,
+        build = ':TSUpdate',
+        config = function()
+            require'nvim-treesitter'.setup{}
+            require'nvim-treesitter.configs'.setup{
+                highlight = {
+                    enable = true,
+                    disable = function(lang, buf)
+                        local clients = vim.lsp.get_clients({ bufnr = buf })
+                        for _, client in ipairs(clients) do
+                            if client.server_capabilities.semanticTokensProvider then
+                                return true -- Disable Tree-sitter highlighting if LSP provides semantic tokens
+                            end
                         end
-                    end
-                end,
-            },
-            incremental_selection = { enable = false },
-            indent = { enable = false },  -- again, rely on LSP-based formatting
-        }
-    end
+                    end,
+                },
+                incremental_selection = { enable = false },
+                indent = { enable = false },  -- again, rely on LSP-based formatting
+            }
+        end
+    }
 }
