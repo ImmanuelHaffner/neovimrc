62e42520f07f1ef09aeee93f0e15d1675bc7db8f
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-08-13 21:13:27 +0300

    Big incline refactor to avoid deep nesting structure

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -1,3 +1,231 @@
+local renderer = { }
+
+function renderer.init()
+    renderer.helpers = require'incline.helpers'
+    renderer.devicons = require'nvim-web-devicons'
+    renderer.colors = require'theme'.colors()
+end
+
+--- Returns a table with the icon's glyph, highlight group, foreground color, and background color.
+--- @param filename string|nil
+--- @param filetype string|nil
+--- @return table
+function renderer.get_icon(filename, filetype)
+    local devicons = renderer.devicons
+    local helpers = renderer.helpers
+
+    -- Try getting by *filetype*.
+    -- local ft_icon, ft_color = devicons.get_icon_color_by_filetype(filetype)
+    -- -- If getting by *filetype* failed, try getting by *filename*.
+    -- if ft_icon == nil then
+    --     ft_icon, ft_color = devicons.get_icon_color(filename)
+    -- end
+
+    local glyph, color = devicons.get_icon_color(filename, filetype)
+    if glyph == nil then
+        return {}
+    end
+    local icon_name = devicons.get_icon_name_by_filetype(filetype)
+
+    -- Try to find a DevIcon highlight group for the filetype
+    if icon_name ~= nil and icon_name ~= '' then
+        local hl_group = 'DevIcon' .. icon_name
+        if vim.fn.hlexists(hl_group) == 1 then
+            local syn_id = vim.fn.synIDtrans(vim.fn.hlID(hl_group))
+            local bg = vim.fn.synIDattr(syn_id, 'bg')
+            if bg ~= '' then
+                local fg = vim.fn.synIDattr(syn_id, 'fg')
+                return {
+                    glyph = glyph,
+                    hl = hl_group,
+                    fg = fg,
+                    bg = bg,
+                }
+            end
+        end
+    end
+
+    return {
+        glyph = glyph,
+        bg = color,
+        fg = helpers.contrast_color(color),
+    }
+end
+
+function renderer.make_ft_icon(filename, filetype)
+    local devicons = renderer.devicons
+
+    local icon = renderer.get_icon(filename, filetype)
+    if icon.glyph == nil then
+        return {}
+    end
+
+    local icon_name = devicons.get_icon_name_by_filetype(filetype)
+    if icon_name ~= nil and icon_name ~= '' then
+        local hl_group = ('DevIcon%s'):format(icon_name)
+        if vim.fn.hlexists(hl_group) == 1 then
+            local syn_id = vim.fn.synIDtrans(vim.fn.hlID(hl_group))
+            local bg = vim.fn.synIDattr(syn_id, 'bg')
+            if bg ~= '' then
+                local fg = vim.fn.synIDattr(syn_id, 'fg')
+                return { ' ', icon.glyph, ' ', guifg = fg, guibg = bg }
+            end
+        end
+    end
+    return { ' ', icon.glyph, ' ', guibg = icon.bg, guifg = icon.fg }
+end
+
+function renderer.render(props)
+    -- Make modules easily accessible
+    local colors = renderer.colors
+
+    -- Hide incline if the cursor is in the top column and the column is too wide.
+    local wininfo = vim.fn.getwininfo(props.win)[1]
+    local text_width = wininfo.width - wininfo.textoff
+
+    local buftype = vim.api.nvim_get_option_value('buftype', { buf = props.buf })
+    local filetype = vim.api.nvim_get_option_value('filetype', { buf = props.buf })
+    local file_path = vim.api.nvim_buf_get_name(props.buf)
+    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+
+    local function get_incline_width()
+        local INCLINE_FIXED_WIDTH = 22
+        return filename:len() + INCLINE_FIXED_WIDTH
+    end
+
+    local lines = vim.api.nvim_buf_get_lines(props.buf, wininfo.topline - 1, wininfo.topline, false)
+    if lines and next(lines) then  -- has content
+        if lines[1]:len() + get_incline_width() > text_width then  -- line too wide
+            local _, linenum, colnum, _, _ = table.unpack(vim.fn.getcurpos(props.win))
+            local num_lines_displayed = wininfo.botline - wininfo.topline + 1
+            if linenum - wininfo.topline < num_lines_displayed / 3 then  -- cursor in upper third of win
+                if colnum > wininfo.width / 2 then  -- cursor not in the front half
+                    return ''
+                end
+            end
+        end
+    end
+
+    -- Special handling of specific file and buffer types.
+    if buftype == 'quickfix' then
+        return {
+            { '', guifg = colors.cyan2, guibg = colors.bg },
+            { ' Quickfix list ', guibg = colors.cyan2, guifg = colors.dark, gui = 'bold' },
+            guibg = colors.blue14,
+        }
+    elseif buftype == 'help' then
+        local icon = renderer.get_icon('vim')
+        return {
+            { '', guifg = icon.bg, guibg = colors.bg },
+            { ' ' .. icon.glyph .. ' HELP ', guibg = icon.bg, guifg = colors.dark, gui = 'bold' },
+            { ' ', guifg = icon.bg, guibg = colors.blue14 },
+            { filename, gui = 'bold,italic' },
+            ' ',
+            guibg = colors.blue14,
+        }
+    elseif buftype == 'terminal' then
+        local toggle_term = string.match(file_path, [[;#toggleterm#(%d+)$]])
+
+        if toggle_term then
+            if string.find(file_path, 'lazygit;#toggleterm#') then
+                local icon = renderer.get_icon('git')
+                return {
+                    { '', guifg = icon.bg, guibg = colors.bg },
+                    { ' ' .. icon.glyph .. ' Lazygit ', guibg = icon.bg, guifg = colors.dark, gui = 'bold' },
+                    -- { 'Lazygit', guibg = icon.guibg, guifg = colors.dark, gui = 'bold' },
+                    { '', guifg = icon.bg, guibg = colors.bg },
+                    { '─╮', guibg = colors.bg, guifg = colors.dark_white },
+                    guibg = colors.blue14,
+                }
+            end
+
+            local icon = renderer.get_icon('bash')
+            return {
+                { '', guifg = icon.bg, guibg = colors.bg },
+                { ' ' .. icon.glyph .. ' ', guibg = icon.bg, guifg = icon.fg },
+                { ('TERMINAL [%s] '):format(toggle_term), guibg = icon.bg, guifg = colors.dark, gui = 'bold' },
+                guibg = colors.blue14,
+            }
+        end
+
+        local icon = renderer.get_icon('bash')
+        return {
+            { '', guifg = icon.bg, guibg = colors.bg },
+            { ' ' .. icon.glyph .. ' TERMINAL ', guibg = icon.bg, guifg = colors.dark, gui = 'bold' },
+            guibg = colors.blue14,
+        }
+    elseif buftype == 'nofile' and filetype == 'noice' then
+        return {
+            { '', guifg = colors.light_red, guibg = colors.bg },
+            { ' Noice ', guifg = colors.dark, guibg = colors.light_red, gui = 'bold' },
+            guibg = colors.blue14,
+        }
+    end
+    if filename == '' then
+        return {
+            { ' [No Name] ', guifg = colors.dark_white },
+            guibg = colors.blue14,
+        }
+    end
+
+    local function get_modified()
+        local modified = vim.bo[props.buf].modified
+        if modified then
+            return ' ✎'
+        end
+        return ''
+    end
+
+    local function get_diagnostic_label()
+        local icons = { error = '', warn = '', info = '', hint = '' }
+        local labels = {}
+
+        for severity, icon in pairs(icons) do
+            local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+            if n > 0 then
+                table.insert(labels, { ' ' .. icon .. n, group = 'DiagnosticSign' .. severity })
+            end
+        end
+        return labels
+    end
+
+    local function get_git_diff()
+        local icons = {
+            { name = 'added',   symbol = '+', fg = colors.sign_add, },  -- Green plus for added lines
+            { name = 'changed', symbol = '~', fg = colors.sign_change, },  -- Yellow tilde for modified lines
+            { name = 'removed', symbol = '-', fg = colors.sign_delete },  -- Red minus for deleted lines
+        }
+
+        local signs = vim.b[props.buf] and vim.b[props.buf].gitsigns_status_dict or nil
+        local labels = {}
+
+        if not signs then
+            return labels
+        end
+
+        for _, config in ipairs(icons) do
+            if tonumber(signs[config.name]) and signs[config.name] > 0 then
+                table.insert(labels, {
+                    ' ' .. config.symbol .. signs[config.name],
+                    guifg = config.fg,
+                })
+            end
+        end
+
+        return labels
+    end
+
+    return {
+        renderer.make_ft_icon(filename, filetype),
+        { ' ', filename, gui = 'bold,italic' },
+        get_modified(),
+        get_diagnostic_label(),
+        get_git_diff(),
+        ' ',
+        guibg = colors.blue14,
+    }
+end
+
 return {
     { 'b0o/incline.nvim',
         dependencies = {
@@ -5,9 +233,7 @@ return {
             'lewis6991/gitsigns.nvim',
         },
         config = function()
-            local helpers = require 'incline.helpers'
-            local devicons = require 'nvim-web-devicons'
-            local colors = require'theme'.colors()
+            renderer.init()
             require('incline').setup{
                 window = {
                     padding = 0,
@@ -39,158 +265,7 @@ return {
                         return ignore == nil or ignore
                     end
                 },
-                render = function(props)
-                    -- Hide incline if the cursor is in the top column and the column is too wide.
-                    local wininfo = vim.fn.getwininfo(props.win)[1]
-                    local text_width = wininfo.width - wininfo.textoff
-
-                    local function get_ft_icon(filename, filetype)
-                        local ft_icon, ft_color = devicons.get_icon_color_by_filetype(filetype)
-                        local icon_name = devicons.get_icon_name_by_filetype(filetype)
-                        if ft_icon == nil then
-                            ft_icon, ft_color = devicons.get_icon_color(filename)
-                        end
-                        if ft_icon == nil then
-                            return {}
-                        end
-                        if icon_name ~= nil and icon_name ~= '' then
-                            local hl_group = ('DevIcon%s'):format(icon_name)
-                            if vim.fn.hlexists(hl_group) == 1 then
-                                local syn_id = vim.fn.synIDtrans(vim.fn.hlID(hl_group))
-                                local bg = vim.fn.synIDattr(syn_id, 'bg')
-                                if bg ~= '' then
-                                    local fg = vim.fn.synIDattr(syn_id, 'fg')
-                                    return {
-                                        { ' ', ft_icon, ' ', guifg = fg, guibg = bg },
-                                    }
-                                end
-                            end
-                        end
-                        return {
-                            { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) },
-                        }
-                    end
-
-                    -- vim.print(('Render %s'):format(vim.inspect(props)))
-                    local buftype = vim.api.nvim_get_option_value('buftype', { buf = props.buf })
-                    local filetype = vim.api.nvim_get_option_value('filetype', { buf = props.buf })
-                    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
-
-                    -- Special handling of specific file and buffer types.
-                    if buftype == 'quickfix' then
-                        return {
-                            { '', guifg = colors.cyan2, guibg = colors.bg },
-                            { ' Quickfix list ', guibg = colors.cyan2, guifg = colors.dark, gui = 'bold' },
-                            guibg = colors.blue14,
-                        }
-                    elseif buftype == 'help' then
-                        local icon = get_ft_icon('vim')[1]
-                        return {
-                            { '', guifg = icon.guibg, guibg = colors.bg },
-                            icon,
-                            { 'HELP ', guibg = icon.guibg, guifg = colors.dark, gui = 'bold' },
-                            { ' ', guifg = icon.guibg, guibg = colors.blue14 },
-                            { filename, gui = 'bold,italic' },
-                            ' ',
-                            guibg = colors.blue14,
-                        }
-                    elseif buftype == 'terminal' then
-                        local icon = get_ft_icon('bash')[1]
-                        return {
-                            { '', guifg = icon.guibg, guibg = colors.bg },
-                            icon,
-                            { 'TERMINAL ', guibg = icon.guibg, guifg = colors.dark, gui = 'bold' },
-                            guibg = colors.blue14,
-                        }
-                    elseif buftype == 'nofile' and filetype == 'noice' then
-                        return {
-                            { '', guifg = colors.light_red, guibg = colors.bg },
-                            { ' Noice ', guifg = colors.dark, guibg = colors.light_red, gui = 'bold' },
-                            guibg = colors.blue14,
-                        }
-                    end
-                    if filename == '' then
-                        return {
-                            { ' [No Name] ', guifg = colors.dark_white },
-                            guibg = colors.blue14,
-                        }
-                    end
-
-                    local function get_incline_width()
-                        local INCLINE_FIXED_WIDTH = 22
-                        return filename:len() + INCLINE_FIXED_WIDTH
-                    end
-
-                    local lines = vim.api.nvim_buf_get_lines(props.buf, wininfo.topline - 1, wininfo.topline, false)
-                    if lines and next(lines) then  -- has content
-                        if lines[1]:len() + get_incline_width() > text_width then  -- line too wide
-                            local _, linenum, colnum, _, _ = table.unpack(vim.fn.getcurpos(props.win))
-                            local num_lines_displayed = wininfo.botline - wininfo.topline + 1
-                            if linenum - wininfo.topline < num_lines_displayed / 3 then  -- cursor in upper third of win
-                                if colnum > wininfo.width / 2 then  -- cursor not in the front half
-                                    return ''
-                                end
-                            end
-                        end
-                    end
-
-                    local function get_modified()
-                        local modified = vim.bo[props.buf].modified
-                        if modified then
-                            return ' ✎'
-                        end
-                        return ''
-                    end
-
-                    local function get_diagnostic_label()
-                        local icons = { error = '', warn = '', info = '', hint = '' }
-                        local labels = {}
-
-                        for severity, icon in pairs(icons) do
-                            local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
-                            if n > 0 then
-                                table.insert(labels, { ' ' .. icon .. n, group = 'DiagnosticSign' .. severity })
-                            end
-                        end
-                        return labels
-                    end
-
-                    local function get_git_diff()
-                        local icons = {
-                            { name = 'added',   symbol = '+', fg = colors.sign_add, },  -- Green plus for added lines
-                            { name = 'changed', symbol = '~', fg = colors.sign_change, },  -- Yellow tilde for modified lines
-                            { name = 'removed', symbol = '-', fg = colors.sign_delete },  -- Red minus for deleted lines
-                        }
-
-                        local signs = vim.b[props.buf] and vim.b[props.buf].gitsigns_status_dict or nil
-                        local labels = {}
-
-                        if not signs then
-                            return labels
-                        end
-
-                        for _, config in ipairs(icons) do
-                            if tonumber(signs[config.name]) and signs[config.name] > 0 then
-                                table.insert(labels, {
-                                    ' ' .. config.symbol .. signs[config.name],
-                                    guifg = config.fg,
-                                })
-                            end
-                        end
-
-                        return labels
-                    end
-
-                    return {
-                        get_ft_icon(filename, vim.bo[props.buf].filetype),
-                        { ' ', filename, gui = 'bold,italic' },
-                        get_modified(),
-                        get_diagnostic_label(),
-                        get_git_diff(),
-                        ' ',
-                        guibg = colors.blue14,
-                    }
-                end,
+                render = renderer.render,
             }
         end,
     },
