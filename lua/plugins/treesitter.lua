fe7f4c8c5ee79e1d9fbdac899b6cad0c16c2c0b1
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-11-11 09:00:53 +0100

    Detect whether tree-sitter CLI is available
    and install grammars needing CLI only if available (currently only latex).

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,19 @@
+local function has_tree_sitter_cli()
+    -- Check if tree-sitter CLI is available and working
+    local handle = io.popen('tree-sitter --version 2>/dev/null')
+    if not handle then
+        return false
+    end
+
+    local result = handle:read('*a')
+    local success = handle:close()
+
+    -- Check if command executed successfully and returned version info
+    local is_tree_sitter_cli_available = success and result and result:match('tree%-sitter') ~= nil
+    vim.notify('tree-sitter CLI available? ' .. tostring(is_tree_sitter_cli_available))
+    return is_tree_sitter_cli_available
+end
+
 return {
     { 'nvim-treesitter/nvim-treesitter',
         dependencies = {
@@ -6,8 +22,50 @@ return {
         lazy = false,
         build = ':TSUpdate',
         config = function()
-            require'nvim-treesitter'.setup{}
+            local ensure_installed = {
+                'awk',
+                'bash',
+                'bibtex',
+                'c',
+                'cmake',
+                'cpp',
+                'diff',
+                'dockerfile',
+                'html',
+                'htmldjango',
+                'lua',
+                'markdown',
+                'markdown_inline',
+                'nginx',
+                'ninja',
+                'python',
+                'query',
+                'regex',
+                'requirements',
+                'rust',
+                'sql',
+                'vim',
+                'vimdoc',
+                'xcompose',
+                'xresources',
+                'yaml',
+                'zathurarc',
+            }
+
+            if has_tree_sitter_cli() then
+                ensure_installed = {
+                    table.unpack(ensure_installed),
+                    -- List of grammars that require the tree-sitter CLI
+                    'latex'
+                }
+            end
+
+            require'nvim-treesitter'.setup()
+            ---@diagnostic disable-next-line: missing-fields
             require'nvim-treesitter.configs'.setup{
+                sync_install = false,
+                auto_install = true,
+                ignore_install = {},
                 highlight = {
                     enable = true,
                     disable = function(lang, buf)
@@ -21,36 +79,7 @@ return {
                 },
                 incremental_selection = { enable = false },
                 indent = { enable = false },  -- again, rely on LSP-based formatting
-                ensure_installed = {
-                    'awk',
-                    'bash',
-                    'bibtex',
-                    'c',
-                    'cmake',
-                    'cpp',
-                    'diff',
-                    'dockerfile',
-                    'html',
-                    'htmldjango',
-                    'latex',
-                    'lua',
-                    'markdown',
-                    'markdown_inline',
-                    'nginx',
-                    'ninja',
-                    'python',
-                    'query',
-                    'regex',
-                    'requirements',
-                    'rust',
-                    'sql',
-                    'vim',
-                    'vimdoc',
-                    'xcompose',
-                    'xresources',
-                    'yaml',
-                    'zathurarc',
-                },
+                ensure_installed = ensure_installed,
             }
         end
     }
