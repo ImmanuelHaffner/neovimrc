bcff77be84f836ccbb7bf44b80d319148189eb74
Immanuel Haffner <haffner.immanuel@gmail.com>
2026-01-06 10:39:56 +0000

    Implement custom telescope buffer previewer with file path shortening

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,76 @@
 local Utils = require 'utils'
 
+local jump_to_line = function(self, bufnr, entry)
+    local utils = require 'telescope.utils'
+    local ns_previewer = vim.api.nvim_create_namespace 'telescope.previewers'
+    pcall(vim.api.nvim_buf_clear_namespace, bufnr, ns_previewer, 0, -1)
+
+    if entry.lnum and entry.lnum > 0 then
+        local lnum, lnend = entry.lnum - 1, (entry.lnend or entry.lnum) - 1
+
+        local col, colend = 0, -1
+        -- Both col delimiters should be provided for them to take effect.
+        -- This is to ensure that column range highlighting was opted in, as `col`
+        -- is already used to determine the buffer jump position elsewhere.
+        if entry.col and entry.colend then
+            col, colend = entry.col - 1, entry.colend - 1
+        end
+
+        for i = lnum, lnend do
+            pcall(
+                utils.hl_range,
+                bufnr,
+                ns_previewer,
+                "TelescopePreviewLine",
+                { i, i == lnum and col or 0 },
+                { i, i == lnend and colend or -1 }
+            )
+        end
+
+        local middle_ln = math.floor(lnum + (lnend - lnum) / 2)
+        pcall(vim.api.nvim_win_set_cursor, self.state.winid, { middle_ln + 1, 0 })
+        if bufnr ~= nil then
+            vim.api.nvim_buf_call(bufnr, function()
+                vim.cmd "norm! zz"
+            end)
+        end
+    end
+end
+
+local function buffer_previewer(self, entry)
+    local from_entry = require 'telescope.from_entry'
+    local conf = require'telescope.config'.values
+
+    -- builtin.buffers: bypass path validation for terminal buffers that don't have appropriate path
+    local has_buftype = entry.bufnr and vim.api.nvim_buf_is_valid(entry.bufnr) and vim.bo[entry.bufnr].buftype ~= ""
+    or false
+    local p
+    if not has_buftype then
+        p = from_entry.path(entry, true, false)
+        if p == nil or p == "" then
+            return
+        end
+    end
+
+    -- Workaround for unnamed buffer when using builtin.buffer
+    if entry.bufnr and (p == "[No Name]" or has_buftype) then
+        local lines = vim.api.nvim_buf_get_lines(entry.bufnr, 0, -1, false)
+        vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
+        -- schedule so that the lines are actually there and can be jumped onto when we call jump_to_line
+        vim.schedule(function()
+            jump_to_line(self, self.state.bufnr, entry)
+        end)
+    else
+        conf.buffer_previewer_maker(p, self.state.bufnr, {
+            bufname = self.state.bufname,
+            winid = self.state.winid,
+            callback = function(bufnr)
+                jump_to_line(self, bufnr, entry)
+            end,
+        })
+    end
+end
+
 return {
     { 'nvim-telescope/telescope-fzf-native.nvim',
         build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 && cmake --build build --config Release && cmake --install build --prefix build',
@@ -16,6 +87,7 @@ return {
             local ts = require'telescope'
             local entry_display = require'telescope.pickers.entry_display'
             local builtins = require'telescope.builtin'
+            local from_entry = require 'telescope.from_entry'
             local previewers = require'telescope.previewers'
             local devicons = require'nvim-web-devicons'
 
@@ -108,10 +180,30 @@ return {
                         hidden = true,
                         no_ignore = true,
                         entry_maker = custom_file_entry_maker{max_len = 80},
+                        previewer = previewers.new_buffer_previewer{
+                            define_preview = buffer_previewer,
+                            get_buffer_by_name = function(_, entry)
+                                return from_entry.path(entry, false, false)
+                            end,
+                            title = 'Find files',
+                            dyn_title = function(self, entry)
+                                return Utils.shorten_relative_path(entry.value, 100)
+                            end,
+                        },
                     },
                     git_files = {
                         prompt_prefix = '  ',
                         entry_maker = custom_file_entry_maker{max_len = 80},
+                        previewer = previewers.new_buffer_previewer{
+                            define_preview = buffer_previewer,
+                            get_buffer_by_name = function(_, entry)
+                                return from_entry.path(entry, false, false)
+                            end,
+                            title = 'Git files',
+                            dyn_title = function(self, entry)
+                                return Utils.shorten_relative_path(entry.value, 100)
+                            end,
+                        },
                     },
                     git_branches = {
                         prompt_prefix = ' ',
