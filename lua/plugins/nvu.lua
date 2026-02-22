67b31e81f9516d76c48a14a2d177ed86a7c6340c
Immanuel Haffner <haffner.immanuel@gmail.com>
2026-02-22 13:02:42 +0100

    Add `nvu` library

---
+++ lua/plugins/nvu.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        'ImmanuelHaffner/nvu.nvim',
+        branch = 'dev',
+        lazy = false,  -- load early
+        priority = 1000,  -- load before other plugins
+    },
+}
