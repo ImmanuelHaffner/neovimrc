18eefdee0358272b275bdedd2f655694069a6672
Immanuel Haffner <haffner.immanuel@gmail.com>
2026-03-14 18:43:32 +0000

    Pin CodeCompanion to last known compatible version
    I will work on upgrading

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -102,6 +102,7 @@ return {
     },
     {
         'olimorris/codecompanion.nvim',
+        tag = 'v18.7.0',
         dependencies = {
             'nvim-lua/plenary.nvim',
             'nvim-treesitter/nvim-treesitter',
