f939c164f61c0a6e1eda6817f91be224c52f116c
Immanuel Haffner <haffner.immanuel@gmail.com>
2026-03-15 18:08:55 +0000

    Formatting

--- lua/plugins/todos.lua
+++ lua/plugins/todos.lua
@@ -1,5 +1,6 @@
 return {
-    { 'folke/todo-comments.nvim',
+    {
+        'folke/todo-comments.nvim',
         dependencies = {
             'nvim-lua/plenary.nvim',
             'folke/which-key.nvim',
