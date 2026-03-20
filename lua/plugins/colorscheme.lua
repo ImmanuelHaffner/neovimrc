b2297e5b652b38f61a1932d6b1b3338fd8927fba
Immanuel Haffner <haffner.immanuel@gmail.com>
2026-03-20 08:50:31 +0100

    Patch night-owl with good markup heading colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -81,6 +81,22 @@ return {
                     hl(0, 'NotifyINFOBody', { fg = palette.blue, bg = palette.bg, nocombine = true })
                     hl(0, 'NotifyDEBUGBody', { fg = palette.cyan2, bg = palette.bg, nocombine = true })
                     hl(0, 'NotifyTRACEBody', { fg = palette.magenta, bg = palette.bg, nocombine = true })
+
+                    -- Distinct foreground colors for markview callout types
+                    hl(0, "@markup.heading.1.markdown", { fg = "#ff5370" })  -- red
+                    hl(0, "@markup.heading.2.markdown", { fg = "#f78c6c" })  -- orange
+                    hl(0, "@markup.heading.3.markdown", { fg = "#ffcb6b" })  -- yellow
+                    hl(0, "@markup.heading.4.markdown", { fg = "#c3e88d" })  -- green
+                    hl(0, "@markup.heading.5.markdown", { fg = "#89ddff" })  -- cyan
+                    hl(0, "@markup.heading.6.markdown", { fg = "#c792ea" })  -- purple
+
+                    -- With background variants for filled callout styles
+                    hl(0, "MarkviewPalette0", { fg = "#969595", bg = "#1e1e2e" })
+                    hl(0, "MarkviewPalette1", { fg = "#f38ba8", bg = "#2a1f29" })
+                    hl(0, "MarkviewPalette2", { fg = "#f9e2af", bg = "#2a2520" })
+                    hl(0, "MarkviewPalette3", { fg = "#cba6f7", bg = "#25202e" })
+                    hl(0, "MarkviewPalette4", { fg = "#a6e3a1", bg = "#1e2a22" })
+                    hl(0, "MarkviewPalette5", { fg = "#89b4fa", bg = "#1a2332" })
                 end,
             })
 
